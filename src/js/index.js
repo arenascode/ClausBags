78bd4e8aae7667759ce0b9d1b8ba8ed04b1f168e
Miguel Arenas <migue.arenas91@gmail.com>
2023-11-02 21:20:30 -0500

    mobile menu in process

--- src/js/index.js
+++ src/js/index.js
@@ -439,4 +439,38 @@ function deleteProductInCart(e) {
   totalCart()
 }
 
-//*CustomizeProductOnWtsp */
+//*Show and hidde MobileMenu Modal */
+const mobileMenu = document.getElementById("navBar_mobileMenu");
+console.log(mobileMenu);
+const hamburguerBtn = document.getElementById("hamburger-btn");
+console.log(hamburguerBtn);
+const closeModalBtn = document.getElementById("closeModalBtn");
+console.log(closeModalBtn);
+
+function openModalMenu() {
+  mobileMenu.style.display = "block";
+  setTimeout(() => {
+    mobileMenu.style.opacity = 1;
+  }, 10);
+}
+
+function closeModalMenu() {
+  console.log(`hiii`);
+  mobileMenu.style.opacity = 0;
+  setTimeout(() => {
+    mobileMenu.style.display = "none";
+  }, 500);
+}
+
+window.onclick = function (e) {
+  if (e.target === mobileMenu) {
+    mobileMenu.style.opacity = 0;
+    setTimeout(() => {
+      mobileMenu.style.display = "none";
+    }, 500);
+  }
+};
+
+hamburguerBtn.addEventListener("click", openModalMenu);
+
+closeModalBtn.addEventListener("click", closeModalMenu);
